05185c9f32d36827c2e09cfc444c0ab366b0148b
Rauljouman <rauljouman@gmail.com>
2025-07-11 10:03:03 +0200

    af
    sf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,15 @@
-// Footer.jsx
 import React from "react";
 import "./styles/Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-<<<<<<< HEAD
-import { faEnvelope, faFile } from "@fortawesome/free-solid-svg-icons";
-=======
 import { faEnvelope, faFilePdf } from "@fortawesome/free-solid-svg-icons";
->>>>>>> origin/main
-
 
 const Footer = () => {
   return (
     <footer className="footer">
       <div>
         <div className="social-icons">
-          <a href="mailto:rauljouman@gmail..com" target="_blank" rel="noopener noreferrer">
+          <a href="mailto:rauljouman@gmail.com" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faEnvelope} />
           </a>
           <a href="https://github.com/Rauljouman" target="_blank" rel="noopener noreferrer">
@@ -24,21 +18,11 @@ const Footer = () => {
           <a href="https://linkedin.com/in/raul-jouman-ip" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
-<<<<<<< HEAD
-          <a
-            href="/CVRaulJouman.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Descargar CV"
-          >
-            <FontAwesomeIcon icon={faFile} />
-=======
           <a href="/CV_Raul_Jouman_Ip.pdf" target="_blank" rel="noopener noreferrer">
             <FontAwesomeIcon icon={faFilePdf} />
->>>>>>> origin/main
           </a>
         </div>
-        <p>&copy; 2025 Raül Jouman.<br/> Todos los derechos resevados.</p>
+        <p>&copy; 2025 Raül Jouman.<br />Todos los derechos reservados.</p>
       </div>
     </footer>
   );
